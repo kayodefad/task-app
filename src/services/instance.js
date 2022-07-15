be5f33e17b9cbb6d70ff7365fe487fad830f737a
Kayode Fadoju <kayode.fadoju@thriveagric.com>
2022-07-15 09:56:13 +0100

    Add all files

--- src/services/instance.js
+++ src/services/instance.js
@@ -12,10 +12,9 @@ admin.defaults.headers.post["Accept"] = "application/json";
 
 admin.interceptors.request.use(
 	(config) => {
-		const token = authService.getToken();
-		if (token) {
-			config.headers["Authorization"] = `Bearer ${token}`;
-		}
+		config.headers[
+			"Authorization"
+		] = `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NTc4NzUxNDgsIm5iZiI6MTY1Nzg3NTE0OCwianRpIjoiZmI4MDUyYjUtNDVlZC00NGI0LWE5MTktN2RkMzY5MjZkNzI3IiwiaWRlbnRpdHkiOnsibmFtZSI6IlN1bmRhciBQaWNoYWkiLCJlbWFpbCI6InNtaXRod2lsbHMxOTg5QGdtYWlsLmNvbSIsInVzZXJfaWQiOiJ1c2VyXzRlZTRjZjY3YWQ0NzRhMjc5ODhiYzBhZmI4NGNmNDcyIiwiaWNvbiI6Imh0dHA6Ly93d3cuZ3JhdmF0YXIuY29tL2F2YXRhci9jZjk0Yjc0YmQ0MWI0NjZiYjE4NWJkNGQ2NzRmMDMyYj9kZWZhdWx0PWh0dHBzJTNBJTJGJTJGczMuc2xvb3ZpLmNvbSUyRmF2YXRhci1kZWZhdWx0LWljb24ucG5nIiwiYnlfZGVmYXVsdCI6Im91dHJlYWNoIn0sImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.EcGSWIN8mYfWLxrxUrFAX43ROudbyOmv1D985EFXMTo`;
 		return config;
 	},
 	(err) => {
